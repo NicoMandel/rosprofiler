8883f66e6c63be615b0c7fd8e28eab5e5869c85a
Nicolas Mandel <nicolasjohann.mandel@hdr.qut.edu.au>
2020-04-15 11:51:15 +1000

    Monitoring Client v1.5 [Nano] - address and name resolution attempt v6

--- src/rosprofiler/profiler_Nano.py
+++ src/rosprofiler/profiler_Nano.py
@@ -93,7 +93,7 @@ class Profiler(object):
             self._host = rosgraph.network.get_host_name()
 
             for key in host_dict.keys():
-                if (key in ip for ip in _local_ip) or (key.lower() in self._host.lower()):
+                if (key in _local_ip) or (key.lower() in self._host.lower()):
                     self._host_monitor = HostMonitor()
 
                     self.nodelist = host_dict[key]
