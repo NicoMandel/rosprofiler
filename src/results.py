34aa40408d01fd34f7b66180977fba602ce18936
Nicolas Mandel <nicolasjohann.mandel@hdr.qut.edu.au>
2020-04-22 17:22:38 +1000

    Monitoring Client v3.0.2 [Nano] - results processing

--- src/results.py
+++ src/results.py
@@ -3,7 +3,9 @@
 import os
 import seaborn as sns
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
 
 def getDataframe(filename):
@@ -20,23 +22,145 @@ def getDataframe(filename):
     except FileNotFoundError as e:
         raise e
 
-def plot_df(df):
+def plot_host_df(df):
     """
-    Function for plotting a single df
+    Function for plotting a single host df.
+    Values in a host_df:
+        "Time", "Duration", "Samples", "CPU Count", "Power",
+        "CPU Load mean", "CPU Load max", "CPU Load std",
+        "Used Memory mean", "Used Memory max", "Used Memory std",
+        "Available Memory mean", "Available Memory min", "Available Memory std", 
+        "Shared Memory mean", "Shared Memory std", "Shared Memory max",
+        "Swap Available mean", "Swap Available std", "Swap Available min",
+        "Swap Used mean", "Swap Used std", "Swap Used max"  
     """
-    fig, axes = plt.subplots(2, 2, sharex=True)
-    df_cpu = df.filter(regex='CPU')
-    df_avail = df.filter(regex='Available')
-    df_used = df.filter(regex='Used')
-    used_cols = df_cpu.columns.values.tolist() + df_avail.columns.values.tolist() + df_used.columns.values.tolist()
+
+    fig, axes = plt.subplots(3, 2, sharex=True)
+    df_cpu = df.filter(regex='CPU L')
+    df_sw_avail = df.filter(regex='Swap Av')
+    df_mem_avail = df.filter(regex='Available Mem')
+    df_mem_used = df.filter(regex='Used Mem')
+    df_sw_used = df.filter(regex='Swap Use')
+    df_mem_shared = df.filter(regex='Shared')
+    used_cols = df_cpu.columns.values.tolist() + df_mem_avail.columns.values.tolist() + df_mem_used.columns.values.tolist() + df_sw_avail.columns.values.tolist() \
+        + df_sw_used.columns.values.tolist() + df_mem_shared.columns.values.tolist()
     df_leftover = df.drop(labels=used_cols, axis=1)
     # df_memory = pd.concat([df_mem, df_swap], sort=False)
-    sns.lineplot(data=df_avail, legend="full", ax=axes[0, 0])       #  dashes=False
-    sns.lineplot(data=df_used, legend="full", ax=axes[0, 1])
-    sns.lineplot(data=df_cpu, legend="full", ax=axes[1, 0])
-    sns.lineplot(data=df_leftover, legend="full", ax=axes[1, 1])
+    ax1 = sns.lineplot(data=df_mem_avail, legend="full", ax=axes[0, 0])       #  dashes=False
+    ax1.set(xlabel="Samples", ylabel="in MB")
+    ax1.set_title('Available Memory')
+    ax2 = sns.lineplot(data=df_mem_used, legend="full", ax=axes[0, 1])
+    ax2.set(xlabel="Samples", ylabel="in MB")
+    ax2.set_title('Used Memory')
+    ax3 = sns.lineplot(data=df_cpu, legend="full", ax=axes[1, 0])
+    ax3.set(xlabel="Samples", ylabel="in %")
+    ax3.set_title('CPU Usage')
+    ax4 = sns.lineplot(data=df_mem_shared, legend="full", ax=axes[1, 1])
+    ax4.set(xlabel="Samples", ylabel="in MB")
+    ax4.set_title('Shared Memory')
+    ax5 = sns.lineplot(data=df_sw_used, legend="full", ax=axes[2, 0])
+    ax5.set(xlabel="Samples", ylabel="in MB")
+    ax5.set_title('Used Swap')
+    ax6 = sns.lineplot(data=df_sw_avail, legend="full", ax=axes[2, 1])
+    ax6.set(xlabel="Samples", ylabel="in MB")
+    ax6.set_title('Available Swap')
+    # sns.lineplot(data=df_leftover, legend="full", ax=axes[1, 1])
+    print("Leftover values: {}".format(df_leftover.columns.values.tolist()))
     plt.show()
 
+def summarize_node_df(df_dict):
+    """
+        receives a dictionary of node dataframes, which should be compared.
+        Returns a dictionary of dataframes compiled by similar measures 
+        Values in a node_df:
+            "Time", "Duration", "Samples", "CPU Count",
+            "Threads", "CPU Load mean", "CPU Load max", "CPU Load std",
+            "PSS mean", "PSS std", "PSS max",
+            "Swap Used mean", "Swap Used std", "Swap Used max",
+            "Virtual Memory mean", "Virtual Memory std", "Virtual Memory max"
+    """
+
+    swap_df = pd.DataFrame()
+    cpu_df = pd.DataFrame()
+    virt_df = pd.DataFrame()
+    pss_df = pd.DataFrame()
+    leftover_df = pd.DataFrame()
+  
+    for node, df in df_dict.items():
+        # Drop all std deviation values
+        df.drop(list(df.filter(regex=" std")), axis=1, inplace=True)
+        _sw = df.filter(regex='Swap')
+        _c = df.filter(regex='CPU L')
+        _vir = df.filter(regex='Virtual')
+        _pss = df.filter(regex='PSS')
+        used_cols = _sw.columns.values.tolist() + _c.columns.values.tolist() + _vir.columns.values.tolist() + _pss.columns.values.tolist() 
+        _lefto  = df.drop(labels=used_cols, axis=1)
+        dfs = [_sw, _c, _vir, _pss, _lefto]
+        
+        # turn the names unique with the node dictionary names
+        # clean the indices
+        for _df in dfs:
+            new = node.split('_')[1]+'_'
+            colnames = [new+name for name in _df.columns.values.tolist()]
+            _df.columns = colnames
+            _df.index = np.round(_df.index.to_series().to_numpy(), decimals=1)
+        
+        swap_df = pd.concat([swap_df, _sw], axis=1, sort=False)
+        cpu_df = pd.concat([cpu_df, _c], axis=1, sort=False)
+        virt_df = pd.concat([virt_df, _vir], axis=1, sort=False)
+        pss_df = pd.concat([pss_df, _pss], axis=1, sort=False)
+        leftover_df = pd.concat([leftover_df, _lefto], axis=1, sort=False)
+
+    print("Test")
+    compiled_df = {}
+    compiled_df["Swap"] = swap_df
+    compiled_df["CPU"] = cpu_df
+    compiled_df["Virt"] = virt_df
+    compiled_df["Pss"] = pss_df
+    compiled_df["Leftovers"] = leftover_df
+    return compiled_df
+    
+def plot_node_df_dict(df_dict):
+    """
+     A function which takes a dictionary sorted by nodes and compiles it 
+    """
+    elems = int(len(df_dict)//2)+1
+    # fig = plt.figure()
+    fig, axes = plt.subplots(elems, elems, sharex=True)
+    axes = axes.reshape(-1)
+    for i, (name, df) in enumerate(df_dict.items()):
+        sns.lineplot(data=df, legend="full", dashes=False, ax=axes[i])
+        axes[i].set(xlabel="Samples")
+        axes[i].set_title(name)
+    
+    plt.show()
+    print("Test Done")
+
+
+def compare_dicts(dict_1, dict_2):
+    """
+        Function to compare two dictionaries for the same information in them.
+        Mostly to be used with node dictionaries 
+    """
+    for key_1, df_1 in dict_1.items():
+        for key_2, df_2 in dict_2.items():
+            nname_1 = key_1.split('_')[1:]
+            nname_2 = key_2.split('_')[1:]
+
+
+def filepaths(directory_string, file_string):
+    """
+        Searches from the base directory of the file for the directory with the directory string
+        and in that directory searches for the file string.
+        Returns:: [str] list of full filepaths.
+    """
+    filepath = os.abspath(os.path.dirname(__file__))
+    for root, dirs, files in os.walk(filepath):
+        for f in files:
+            if f.endswith(".csv"):
+                file_id = f.split("_")[-1].split(".")[0]
+                dets = pd.read_csv(os.path.join(root,f),header=None)
+    
 
 if __name__=="__main__":
 
@@ -44,9 +168,11 @@ if __name__=="__main__":
 
     # 1. get the File which results we are interested in
     counter = 1
-    filetype = 'hosts'
+    filetype = 'nodes'
+    result_host = '_pc'
+    # result_host = '_nano'
     parentDir = os.path.dirname(__file__)
-    fname = os.path.abspath(os.path.join(parentDir, '..','results','case_'+str(counter)+'_'+filetype+'.xlsx'))
+    fname = os.path.abspath(os.path.join(parentDir, '..','results'+result_host,'case_'+str(counter)+result_host+'_'+filetype+'.xlsx'))
 
     # 2. Get the dataframe from the filename
     try:
@@ -68,5 +194,7 @@ if __name__=="__main__":
 
     print(first_df.columns)
     # df.columns = df.columns.str.replace(' ', '_')
-    plot_df(first_df)
+    # plot_host_df(first_df)
+    compiled_df = summarize_node_df(df_dict)
+    plot_node_df_dict(compiled_df)
     print("Done")
